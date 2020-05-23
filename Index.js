f7af32125f1f11980050956090f35f4b5c002790
jpd61 <jpd@dewoodyoil.com>
2020-05-23 13:22:47 -0500

    set up requirements in index

---
+++ Index.js
@@ -0,0 +1,7 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const html = require("./src/htmlTemp");
