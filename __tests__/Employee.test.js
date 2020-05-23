872cf28d85042cac7c47ca04f9ba71630ae61355
jpd61 <jpd@dewoodyoil.com>
2020-05-23 12:46:58 -0500

    set up employee tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -11,3 +11,38 @@ test("Set Employee Name", () => {
     expect(emp.name).toBe(name);
 });
 
+test("Set ID", () => {
+    const testValue = 100;
+    const emp = new Employee("Jose", testValue);
+    expect(emp.id).toBe(testValue);
+});
+
+test("Set Email", () => {
+    const testValue = "test@valormm.com";
+    const emp = new Employee("Jose", 1, testValue);
+    expect(emp.email).toBe(testValue);
+});
+
+test("Get name", () => {
+    const testValue = "Joseph";
+    const emp = new Employee(testValue);
+    expect(emp.getName()).toBe(testValue);
+});
+
+test("Get ID", () => {
+    const testValue = 61;
+    cosnt emp = new Employee("Jose", testValue);
+    expect(e.getId()).toBe(testValue);
+});
+
+test("Get Email", () => {
+    const testValue = "test@valormm.com";
+    const emp = new Employee("Jose", 1, testValue);
+    expect(emp.getEmail()).toBe(testValue);
+});
+
+test("Get Role", () => {
+    const testValue = "Employee";
+    const emp = new Employee("Joseph", 1, "test@valormm.com");
+    expect(emp.getRole()).toBe(testValue);
+});
