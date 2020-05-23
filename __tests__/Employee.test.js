3b65d65cbf826e9369d0fc477d808e3b45310552
jpd61 <jpd@dewoodyoil.com>
2020-05-23 16:48:24 -0500

    refactor and comment

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -5,43 +5,43 @@ test("New Employee created", ()=> {
     expect(typeof(emp)).toBe("object");
 });
 
-test("Set Employee Name", () => {
+test("Set Employee Name via constructor argument", () => {
     const name = "Joseph";
     const emp = new Employee(name);
     expect(emp.name).toBe(name);
 });
 
-test("Set ID", () => {
+test("Set ID via constructor argument", () => {
     const testValue = 100;
     const emp = new Employee("Jose", testValue);
     expect(emp.id).toBe(testValue);
 });
 
-test("Set Email", () => {
+test("Set Email via constructor argument", () => {
     const testValue = "test@valormm.com";
     const emp = new Employee("Jose", 1, testValue);
     expect(emp.email).toBe(testValue);
 });
 
-test("Get name", () => {
+test("Get name via getName()", () => {
     const testValue = "Joseph";
     const emp = new Employee(testValue);
     expect(emp.getName()).toBe(testValue);
 });
 
-test("Get ID", () => {
+test("Get ID via getId()", () => {
     const testValue = 61;
     const emp = new Employee("Jose", testValue);
     expect(emp.getId()).toBe(testValue);
 });
 
-test("Get Email", () => {
+test("Get Email via getEmail()", () => {
     const testValue = "test@valormm.com";
     const emp = new Employee("Jose", 1, testValue);
     expect(emp.getEmail()).toBe(testValue);
 });
 
-test("Get Role", () => {
+test("getRole() functions correctly", () => {
     const testValue = "Employee";
     const emp = new Employee("Joseph", 1, "test@valormm.com");
     expect(emp.getRole()).toBe(testValue);
