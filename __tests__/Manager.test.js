cb202628389129a09b30d8d0403bee0ae6097cc5
jpd61 <jpd@dewoodyoil.com>
2020-05-23 12:58:11 -0500

    Manager tests setup

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,20 @@
+const Manager = require("../lib/Manager");
+const Employee = require("../lib/Employee");
+
+test("Set Office Number", () => {
+    const testValue = 61;
+    const emp = new Manager("Jose", 1, "test@valormm.com", testValue);
+    expect(emp.officeNumber).toBe(testValue);
+});
+
+test("Get Role", () => {
+    const testValue = "Manager";
+    const emp = new Manager("Jose", 1, "test@valormm.com");
+    expect(emp.getRole()).toBe(testValue);
+});
+
+test("Get Office Number", () => {
+    const testValue = 61;
+    const emp = new Manager("Jose", 1, "test@valormm.com", testValue);
+    expect(emp.getOfficeNumber()).toBe(testValue):
+});
