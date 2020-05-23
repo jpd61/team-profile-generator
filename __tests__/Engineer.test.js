4f04d23389f7fddb61b359b308a6467517120e56
jpd61 <jpd@dewoodyoil.com>
2020-05-23 13:07:45 -0500

    Engineer tests

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,19 @@
+const Engineer = require("./lib/Engineer");
+
+test("Set GitHub Account", () => {
+    const testValue = "GitHubUser";
+    const emp = new Engineer("Jose", 1, "test@valormm.com", testValue);
+    expect(emp.github).toBe(testValue);
+});
+
+test("Get Role", () => {
+    const testValue = "Engineer";
+    const emp = new Engineer("Jose", 1, "test@valormm.com", "GitHubUser");
+    expect(emp.getRole()).toBe(testValue);
+});
+
+test("Get GitHub UN", () => {
+    const testValue = "GitHubUser";
+    const emp = new Engineer("Jose", 1, "test@valormm.com", testValue);
+    expect(emp.getGitHub()).toBe(testValue);
+});
